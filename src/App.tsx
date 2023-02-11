8ce0ba76509a18712927d1f4d9efe91cd49571ca
Matheus Dias <mthdias@outlook.com>
2023-02-11 12:50:57 -0300

    animate presence for page change transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes, useLocation } from "react-router-dom";
 import Footer from "./features/Footer";
 import NavBar from "./features/NavBar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
+
+  const location = useLocation()
   
   return (
-    <BrowserRouter>
-      <div className="h-screen w-screen flex justify-between flex-col select-none">
+      <div className="h-screen w-screen overflow-x-hidden flex justify-between flex-col select-none">
         <NavBar />
-          <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/about" element={<AboutPage/>}/>
+        <AnimatePresence mode="wait">
+          <Routes key={location.pathname} location={location}>
+            <Route index path="/" element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
           </Routes>
+        </AnimatePresence>
         <Footer />
       </div>
-    </BrowserRouter>
   );
 }
 
