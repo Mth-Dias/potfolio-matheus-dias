9f9a6887896cf4a757674837274e5530e4ebafc6
Matheus Dias <mthdias@outlook.com>
2023-02-10 23:22:54 -0300

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { RouterProvider} from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes} from "react-router-dom";
 import Footer from "./features/Footer";
 import NavBar from "./features/NavBar";
-import RouterHook from "./hooks/RouterHook";
-
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   
-  const { router } = RouterHook()
-
   return (
-    <div className="h-screen w-screen flex justify-between flex-col select-none">
-      <NavBar />
-      <RouterProvider router={router} />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="h-screen w-screen flex justify-between flex-col select-none">
+        <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/about" element={<AboutPage/>}/>
+          </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
