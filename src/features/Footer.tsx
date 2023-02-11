bec85d6e35e761af52e5c697b642abf28756c37d
Matheus Dias <mthdias@outlook.com>
2023-02-10 23:32:54 -0300

    repo url fixed

--- src/features/Footer.tsx
+++ src/features/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return <div className="w-full flex justify-center items-center py-4 flex-col gap-y-2 font-thin text-gray-500">
     <div>Built with React.js && TailwindCSS && FramerMotion</div>
-    <a className="transition-all duration-200 hover:text-black underline" href="https://github.com/Mth-Dias/portfolio" target="_blank">see repo here</a>
+    <a className="transition-all duration-200 hover:text-black underline" href="https://github.com/Mth-Dias/potfolio-matheus-dias" target="_blank">see repo here</a>
   </div>;
 }
