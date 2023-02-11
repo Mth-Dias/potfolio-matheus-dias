f1cccb277141290c81ab74c59900abd8420139c2
Matheus Dias <mthdias@outlook.com>
2023-02-10 23:24:13 -0300

    home page framer motion transitions included and responsive adjusts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,65 @@
+import { HiArrowSmRight } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
 export default function HomePage() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full flex">
-      <div className="group relative lg:-space-x-24 mx-auto flex lg:flex-row flex-col items-center justify-center">
-        <img
-          src="/assets/images/react.png"
-          alt="React_logo"
-          draggable={false}
-          className="w-[60vw] lg:w-[350px] xl:w-[450px] transition-all duration-300  group-hover:lg:w-[400px] group-hover:xl:w-[500px] group-hover:w-[75vw] animate-spin-slow "
-        />
-        <div className="mt-10 z-[1] text-black whitespace-nowrap leading-[30px] lg:leading-[50px] xl:leading-[69px] text-center flex flex-col">
-          <span className="text-[7vw] lg:text-[55px] xl:text-[80px]">Matheus Dias</span>
-          <div className="transition-all duration-300 border-b-2 lg:border-b-4 border-black group-hover:w-[100%] w-[0%]"></div>
-          <div className="mt-2 opacity-0 transition-all duration-300 group-hover:opacity-100 text-2xl">
-            <button>
-              about me
-            </button>
+      <div className="group mx-auto relative flex lg:flex-row flex-col justify-center items-center w-fit">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 3,
+            delay: 0.2,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          drag
+          dragConstraints={{
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        >
+          <img
+            src="/assets/images/react.png"
+            alt="React_logo"
+            draggable={false}
+            className="animate-spin-slow w-[60vw] sm:w-[26rem] cursor-grabbing"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            duration: 1,
+            delay: 1,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          style={{z: 1}}
+        >
+          <div className="lg:-ml-32 lg:mt-[48px]">
+            <span className="text-5xl sm:text-7xl text-center uppercase">
+              Matheus Dias
+            </span>
+            <div className="h-1 bg-black w-[0%] lg:group-hover:w-[100%] transition-all duration-300"></div>
+            <div className="flex justify-center pt-3">
+              <button
+                onClick={() => navigate("/about")}
+                className="flex items-center gap-x-1 lg:text-2xl lg:group-hover:opacity-100 lg:opacity-0 transition-opacity duration-700 delay-75"
+              >
+                <span>about me</span>
+                <span className="pt-[7px]">
+                  <HiArrowSmRight />
+                </span>
+              </button>
+            </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
 }
-5;
