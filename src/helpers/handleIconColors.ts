e5be23386ff307a157ae16f46ae8f904ac82e9d6
Matheus Dias <mthdias@outlook.com>
2023-02-11 12:52:30 -0300

    helper for usage in the tech card component

---
+++ src/helpers/handleIconColors.ts
@@ -0,0 +1,21 @@
+export const handleColors = (iconName:string) => {
+    switch (iconName) {
+        case 'React.js':
+                return 'hover:text-[#61dafb] hover:fill-[#61dafb]'
+        case 'Next.js':
+            return 'hover:text-[#000000] hover:fill-[#000000]'
+        case 'Javascript':
+            return 'hover:text-[#f7df1e] hover:fill-[#f7df1e]'
+        case 'Typescript':
+            return 'hover:text-[#1976d2] hover:fill-[#1976d2]'
+        case 'Tailwind CSS':
+            return 'hover:text-[#06b6d4] hover:fill-[#06b6d4]'
+        case 'Redux Toolkit':
+            return 'hover:text-[#764ABC] hover:fill-[#764ABC]'
+        case 'Jest':
+            return 'hover:text-[#99425b] hover:fill-[#99425b]'
+        case 'Cypress':
+            return 'hover:text-[#4a4a4d] hover:fill-[#4a4a4d]'
+        default:
+    }
+}
