4550ad01e1592f4fe8e89d37b0e07447dd9ca9bf
Matheus Dias <mthdias@outlook.com>
2023-02-11 12:52:17 -0300

    footer responsive adjust

--- src/features/Footer.tsx
+++ src/features/Footer.tsx
@@ -1,6 +1,14 @@
 export default function Footer() {
-  return <div className="w-full flex justify-center items-center py-4 flex-col gap-y-2 font-thin text-gray-500">
-    <div>Built with React.js && TailwindCSS && FramerMotion</div>
-    <a className="transition-all duration-200 hover:text-black underline" href="https://github.com/Mth-Dias/potfolio-matheus-dias" target="_blank">see repo here</a>
-  </div>;
+  return (
+    <div className="w-full flex justify-center items-center py-4 flex-col gap-y-2  text-gray-500 text-center">
+      <div>built with React.js && TailwindCSS && FramerMotion</div>
+      <a
+        className="transition-all duration-200 hover:text-black underline"
+        href="https://github.com/Mth-Dias/potfolio-matheus-dias"
+        target="_blank"
+      >
+        see repo here
+      </a>
+    </div>
+  );
 }
