3313ce43313b8edefc967c6c1b53f21be2893f87
Matheus Dias <mthdias@outlook.com>
2023-02-10 23:23:19 -0300

    Footer component

--- src/features/Footer.tsx
+++ src/features/Footer.tsx
@@ -1,3 +1,6 @@
 export default function Footer() {
-  return <div className="w-full border-t-2 border-black">oi</div>;
+  return <div className="w-full flex justify-center items-center py-4 flex-col gap-y-2 font-thin text-gray-500">
+    <div>Built with React.js && TailwindCSS && FramerMotion</div>
+    <a className="transition-all duration-200 hover:text-black underline" href="https://github.com/Mth-Dias/portfolio" target="_blank">see repo here</a>
+  </div>;
 }
