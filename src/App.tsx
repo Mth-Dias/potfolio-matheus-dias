fdab22fb152a26d391070f8c6652df47a7939e59
Matheus Dias <mthdias@outlook.com>
2023-02-11 19:36:15 -0300

    new paths included experiences and contact pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import { BrowserRouter, Route, Router, Routes, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Footer from "./features/Footer";
 import NavBar from "./features/NavBar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import { AnimatePresence, motion } from "framer-motion";
+import ExperiencesPage from "./pages/ExperiencesPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
+  const location = useLocation();
 
-  const location = useLocation()
-  
   return (
-      <div className="h-screen w-screen overflow-x-hidden flex justify-between flex-col select-none">
-        <NavBar />
-        <AnimatePresence mode="wait">
-          <Routes key={location.pathname} location={location}>
-            <Route index path="/" element={<HomePage />} />
-            <Route path="about" element={<AboutPage />} />
-          </Routes>
-        </AnimatePresence>
-        <Footer />
-      </div>
+    <div className="h-screen w-screen overflow-x-hidden flex justify-between flex-col select-none bg-white">
+      <NavBar />
+      <AnimatePresence mode="wait">
+        <Routes key={location.pathname} location={location}>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="experiences" element={<ExperiencesPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Routes>
+      </AnimatePresence>
+      <Footer />
+    </div>
   );
 }
 
